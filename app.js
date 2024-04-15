e13b4ee67eb55996c296686dd1259f5cec008a22
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-15 03:58:17 -0700

    complete the styling and struncture of the project and start adding functionality in it

---
+++ app.js
@@ -0,0 +1,10 @@
+let hour = document.querySelector(".hour")
+console.log(hour);
+let minute = document.querySelector(".minutes")
+console.log(minute);
+let second = document.querySelector(".seconds")
+console.log(second);
+let pm = document.querySelector(".pm")
+console.log(pm);
+let semiColon = document.querySelector(".semi-colon")
+console.log(semiColon);
