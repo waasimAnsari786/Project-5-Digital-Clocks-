9a319d4beba396d47a8327ace4c40fae68abb666
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-15 20:20:46 -0700

    update app.js for adding programs of clock and blinking of dots

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-let hour = document.querySelector(".hour")
-console.log(hour);
-let minute = document.querySelector(".minutes")
-console.log(minute);
-let second = document.querySelector(".seconds")
-console.log(second);
-let pm = document.querySelector(".pm")
-console.log(pm);
-let semiColon = document.querySelector(".semi-colon")
-console.log(semiColon);
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minutes");
+let second = document.querySelector(".seconds");
+let pm = document.querySelector(".pm");
+let dots = document.querySelectorAll(".semi-colon");
+
+const dotsBlinking = (second) => {
+    if(second % 2 === 0){
+        for (let index = 0; index < dots.length; index++) {
+            dots[index].style.color = `rgb(6, 29, 59)`;
+        }
+    } 
+    else{
+        for (let index = 0; index < dots.length; index++) {
+            dots[index].style.color = `white`;
+        }
+    };
+};
+
+const clock = () => {
+    let h = new Date().getDate();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    if(h > 12) {
+        h -= 12;
+    }
+
+    s =  s < 10 ? `0${s}` : s;
+    m =  m < 10 ? `0${m}` : m;
+    h =  h < 10 ? `0${h}` : h;
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+
+    dotsBlinking(s);
+};
+
+setInterval(clock , 1000);
