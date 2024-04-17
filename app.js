c22d47fcc4ba5b9919dbaeb83fc8db89064d03b9
waasimAnsari786 <waasimansari92@gmail.com>
2024-04-17 05:11:15 -0700

    update index.html, style.css and app.js
    i created a program for gaining the value of date settings.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let pm = document.querySelector(".pm");
 let dots = document.querySelectorAll(".semi-colon");
 let clockDiv = document.querySelector("#clock-div");
 clockDiv.style.boxShadow = `0 0 10rem .1rem black`;
+clockDiv.style.position = `relative`;
 
 let newDiv = document.createElement("div");
 newDiv.classList.add("col-6");
@@ -38,19 +39,32 @@ newDiv.append(year);
 
 let accessNewDiv = document.querySelector("#clock-div").firstElementChild;
 accessNewDiv.append(newDiv);
+accessNewDiv.style.position = `relative`;
 
 let setting = document.createElement("button");
-setting.innerText = `Date and Time`;
+setting.innerText = `Set Date and Time`;
+setting.classList.add("position");
 
 clockDiv.append(setting);
 
+let menu = document.querySelector("#menu");
+menu.classList.add("hide");
+
+let okBtn = document.createElement("button");
+okBtn.innerText = `ok`.toUpperCase();
+okBtn.classList.add("class-of-btns");
+
+let cancelBtn = document.createElement("button");
+cancelBtn.classList.add("class-of-btns");
+cancelBtn.innerText = `cancel`.toUpperCase();
+
 const dotsBlinking = (second) => {
-    if(second % 2 === 0){
+    if (second % 2 === 0) {
         for (let index = 0; index < dots.length; index++) {
             dots[index].style.color = `rgb(6, 29, 59)`;
         }
-    } 
-    else{
+    }
+    else {
         for (let index = 0; index < dots.length; index++) {
             dots[index].style.color = `white`;
         }
@@ -62,17 +76,11 @@ const clock = () => {
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
 
-    if(h > 12) {
-        h -= 12;
-    }
+    h = h > 12 ? h -= 12 : h;
 
-    if(h < 12) {
-        pm.innerText = `am`;
-    };
-
-    s =  s < 10 ? `0${s}` : s;
-    m =  m < 10 ? `0${m}` : m;
-    h =  h < 10 ? `0${h}` : h;
+    s = s < 10 ? `0${s}` : s;
+    m = m < 10 ? `0${m}` : m;
+    h = h < 10 ? `0${h}` : h;
 
     hour.innerText = h;
     minute.innerText = m;
@@ -81,4 +89,30 @@ const clock = () => {
     dotsBlinking(s);
 };
 
-setInterval(clock , 1000);
+setInterval(clock, 1000);
+
+let dateBtn = document.querySelector("#date-btn");
+let timeBtn = document.querySelector("#time-btn");
+
+setting.addEventListener("click", () => {
+    menu.classList.remove("hide");
+    setting.classList.add("hide");
+});
+
+dateBtn.addEventListener("click", () => {
+    menu.classList.add("hide");
+
+    let btnDiv = document.createElement("div");
+    btnDiv.classList.add("d-flex", "justify-content-around", "align-items-center", "w-100", "mt-5")
+    clockDiv.append(btnDiv);
+
+    btnDiv.append(okBtn);
+    btnDiv.append(cancelBtn);
+
+    newDiv.classList.add("col-9");
+
+    let newDate = date.innerHTML = `<input type="number" class="date-input" value="00">`;
+    let newMonth = month.innerHTML = `<input type="number" class="date-input" value="00">`;
+    let newYear = year.innerHTML = `<input type="number" class="date-input" value="00">`;
+    setDates(newDate, newMonth, newYear);
+});
